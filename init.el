3e32adebf7b11b9dd33318a13740bf33ccbb6092
Demi Mgga <demi@bydemi.org>
2020-05-01 21:03:19 +0300

    remove ts

--- init.el
+++ init.el
@@ -7,21 +7,6 @@
 (require 'init-miscellaneous)
 (require 'init-company-mode)
 (require 'init-rust)
-(require 'init-typescript)
 (require 'init-org)
 
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ng2-mode flycheck-rust racer company projectile smex flycheck rainbow-delimiters atom-one-dark-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- lisp/init-typescript.el
+++
@@ -1,8 +0,0 @@
-(require 'init-elpa)
-(require-package 'ng2-mode)
-
-(require 'ng2-mode)
-
-(with-eval-after-load 'typescript-mode (add-hook 'typescript-mode-hook #'lsp))
-
-(provide 'init-typescript)
